4684ed5812e8839cb65b61d209660992a7268447
Paulo Cesar <129324033+PauloCesar0073@users.noreply.github.com>
2023-06-13 15:40:00 -0300

    TimerPomodoro_v1

---
+++ código fonte/scripts/background.js
@@ -0,0 +1,81 @@
+let timerId;
+let minutes;
+let seconds;
+let isPaused = false;
+
+function playAlarm() {
+  let alarm = new Audio(browser.runtime.getURL("alarm.mp3"));
+  alarm.play();
+  showNotification();
+}
+
+function startTimer() {
+  minutes = 25;
+  seconds = 0;
+  isPaused = false;
+  timerId = setInterval(countDown, 1000);
+}
+
+function countDown() {
+  if (!isPaused) {
+    seconds--;
+
+    if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+    }
+
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(timerId);
+      playAlarm();
+    }
+
+    browser.runtime.sendMessage({
+      type: "updateTimer",
+      minutes: minutes,
+      seconds: seconds
+    });
+  }
+}
+
+function pauseTimer() {
+  isPaused = true;
+}
+
+function resumeTimer() {
+  isPaused = false;
+}
+
+function stopTimer() {
+  clearInterval(timerId);
+  minutes = 0;
+  seconds = 0;
+  browser.runtime.sendMessage({
+    type: "updateTimer",
+    minutes: minutes,
+    seconds: seconds
+  });
+}
+
+browser.runtime.onMessage.addListener(function(message) {
+  if (message.type === "startTimer") {
+    startTimer();
+  } else if (message.type === "pauseTimer") {
+    pauseTimer();
+  } else if (message.type === "resumeTimer") {
+    resumeTimer();
+  } else if (message.type === "stopTimer") {
+    stopTimer();
+  }
+});
+
+function showNotification() {
+  browser.notifications.create({
+    type: "basic",
+    iconUrl: browser.runtime.getURL("icon.png"),
+    title: "Pomodoro Timer",
+    message: "O cronômetro zerou!",
+  });
+}
+
+
---
+++ código fonte/scripts/popup.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var startButton = document.getElementById("startButton");
+  var pauseButton = document.getElementById("pauseButton");
+  var resumeButton = document.getElementById("resumeButton");
+  var stopButton = document.getElementById("stopButton");
+  var resizeButton = document.getElementById("resizeButton");
+  var alertDiv = document.getElementById("alert");
+  var alertSound = document.getElementById("alertSound");
+
+  var timerId; // Variável para armazenar o ID do temporizador
+
+  startButton.addEventListener("click", function() {
+    browser.runtime.sendMessage({ type: "startTimer" });
+    alertDiv.textContent = "Pomodoro iniciado!";
+    playAlertSound(); // Reproduzir o som de alerta quando o botão "Iniciar" for pressionado
+    startTimer();
+  });
+
+  pauseButton.addEventListener("click", function() {
+    browser.runtime.sendMessage({ type: "pauseTimer" });
+    clearTimeout(timerId); // Pausar o temporizador
+  });
+
+  resumeButton.addEventListener("click", function() {
+    browser.runtime.sendMessage({ type: "resumeTimer" });
+    startTimer(); // Retomar o temporizador
+  });
+
+  stopButton.addEventListener("click", function() {
+    browser.runtime.sendMessage({ type: "stopTimer" });
+    clearTimeout(timerId); // Parar o temporizador
+    resetTimer();
+  });
+
+  resizeButton.addEventListener("click", function() {
+    resizeWindow(400, 500);
+  });
+
+  // Função para iniciar o temporizador
+function startTimer() {
+  timerId = setInterval(function() {
+    browser.runtime.sendMessage({ type: "tick" });
+  }, 1000); // Alterado para 1000 para 1 segundo
+}
+
+  // Função para redefinir o cronômetro para 25:00
+  function resetTimer() {
+    var timer = document.getElementById("timer");
+    timer.textContent = "25:00";
+  }
+});
+
+browser.runtime.onMessage.addListener(function(message) {
+  if (message.type === "updateTimer") {
+    var timer = document.getElementById("timer");
+    timer.textContent = formatTime(message.minutes) + ":" + formatTime(message.seconds);
+
+    if (message.minutes === 0 && message.seconds === 0) {
+      clearTimeout(timerId); // Parar o temporizador quando chegar a zero
+      playAlertSound(); // Reproduzir o som de alerta quando o cronômetro chegar a zero
+      showNotification("Pomodoro Concluído!"); // Exibir notificação quando o cronômetro chegar a zero
+    }
+  }
+});
+
+function formatTime(time) {
+  return time < 10 ? "0" + time : time;
+}
+
+function resizeWindow(width, height) {
+  browser.windows.getCurrent().then(function(window) {
+    browser.windows.create({
+      url: browser.extension.getURL("popup.html"),
+      type: "popup",
+      width: width,
+      height: height
+    });
+    browser.windows.remove(window.id);
+  });
+}
+
+// Função para reproduzir o som de alerta
+function playAlertSound() {
+  alertSound.currentTime = 0; // Reiniciar o áudio do alerta
+  alertSound.play();
+}
+
+// Função para exibir notificação
+function showNotification(message) {
+  browser.notifications.create({
+    type: "basic",
+    title: "Pomodoro Timer",
+    message: message
+  });
+}
